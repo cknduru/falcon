b639cc0bfc01ff81e3cab872588afa76aecb4a09
Netix <netix@net.com>
2020-03-16 10:40:31 -0900

    Removed unnecessary output from log script
    Signed-off-by: Netix <netix@net.com>

--- launch.sh
+++ launch.sh
@@ -1,15 +1,15 @@
 FOLDER_SERVER='falcon_server'
 FOLDER_CLIENT='falcon_client'
 
-pushd .
+pushd . > /dev/null
 cd $FOLDER_SERVER
 source venv/bin/activate
 screen -dmS flask_server flask run --host=0.0.0.0
-popd
-pushd .
+popd > /dev/null
+pushd . > /dev/null
 cd $FOLDER_CLIENT
 screen -dmS flask_client sudo python3 -m http.server 8080
-popd
+popd > /dev/null
 source ~/.bashrc
 
 sleep 2
