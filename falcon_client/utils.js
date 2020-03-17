7e2862f760225992fa3991ce42edfe72f1e6dcb0
Netix <netix@net.com>
2020-03-17 12:57:46 -0900

    ui misaligned but more simple functions work disable button to prevent spam
    Signed-off-by: Netix <netix@net.com>

--- falcon_client/utils.js
+++ falcon_client/utils.js
@@ -1,27 +1,34 @@
+function toggleVisibility(component, show)
+{
+	document.getElementById(component).style.display = show;
+}
+
+function playMusic()
+{
+	alert("play music");
+}
+
+function toggleLights(placement)
+{
+	alert(`toggling lights at ${placement}`);
+}
+
 const getDevices = async () => 
 {
   	const response = await fetch('http://192.168.87.189:5000');
   	const myJson = await response.json(); //extract JSON from the http response
-  	var btn=document.createElement("button");
+
+  	document.getElementById("btnConnectServer").disabled = true;
+  	
+  	var btn = document.getElementById("dropdownMenuButton");
+  	toggleVisibility("dropdownMenuButton", "block");
 	 
 	// create popover
   	btn.innerHTML = `${myJson.device_name}@${myJson.location}`;
-  	btn.type = "button";
-  	btn.className = "btn btn-primary btn-sm";
-  	btn.id="node_1";
-  	var hover_container = document.getElementById("node_popover");
-  	hover_container.appendChild(btn);
-
-	jQuery("#node_1")
-	 .popover({
-	   trigger: 'click',
-	   title: '',
-	   content: 'Turn on relay',
-	   placement: 'right'
-	});
 }
 
 function fillIPField()
 {
 	document.getElementById("serverField").value = '192.168.87.189:5000';
+	toggleVisibility("dropdownMenuButton", "none");
 }
