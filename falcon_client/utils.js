f26be6a7ca3a4d1677cb0cee8dad9a1a3ee505eb
Netix <netix@net.com>
2020-03-19 07:39:09 -0900

    Added POST routing in server and POST ability in client
    Signed-off-by: Netix <netix@net.com>

--- falcon_client/utils.js
+++ falcon_client/utils.js
@@ -1,3 +1,17 @@
+function getServerIp()
+{
+	return 'http://192.168.87.189:5000';
+}
+
+function sendCommand(cmd)
+{
+	$.ajax({
+	  type: "POST",
+	  url: getServerIp(),
+	  data: {cmd : 'testcmd'},
+	});
+}
+
 function toggleVisibility(component, show)
 {
 	document.getElementById(component).style.display = show;
@@ -5,6 +19,7 @@ function toggleVisibility(component, show)
 
 function playMusic()
 {
+	sendCommand('bah');
 	alert("play music");
 }
 
@@ -13,22 +28,21 @@ function toggleLights(placement)
 	alert(`toggling lights at ${placement}`);
 }
 
-const getDevices = async () => 
+const displayDevices = async () => 
 {
-  	const response = await fetch('http://192.168.87.189:5000');
-  	const myJson = await response.json(); //extract JSON from the http response
+  	const response = await fetch(getServerIp());
+  	const myJson = await response.json(); // extract JSON from the http response
 
   	document.getElementById("btnConnectServer").disabled = true;
   	
+  	// display drop down
   	var btn = document.getElementById("dropdownMenuButton");
   	toggleVisibility("dropdownMenuButton", "block");
-	 
-	// create popover
   	btn.innerHTML = `${myJson.device_name}@${myJson.location}`;
 }
 
 function fillIPField()
 {
-	document.getElementById("serverField").value = '192.168.87.189:5000';
+	document.getElementById("serverField").value = getServerIp();
 	toggleVisibility("dropdownMenuButton", "none");
 }
--- falcon_server/app/routes.py
+++ falcon_server/app/routes.py
@@ -1,6 +1,9 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def node_info():
-    return jsonify(device_name='pi', location='center console')
+	if request.method == 'GET':
+		return jsonify(device_name='pi', location='center console')
+	elif request.method == 'POST':
+		return jsonify(device_name='pi', location='center console')
