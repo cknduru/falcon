b5ffb5c79369ad53196113acb4097a900c3cd9fa
Netix <netix@net.com>
2020-03-26 11:28:49 -0900

    Added comments at backend regarding relays
    Signed-off-by: Netix <netix@net.com>

--- falcon_server/app/relay_util.py
+++ falcon_server/app/relay_util.py
@@ -8,8 +8,14 @@ def setup_relay_data():
 	global initialised
 	global placements
 
+	print('initialising relay data')
+
+	# [0] is pin number [1] is relay object registered at a certian pin
+	# new relay channels are added here
+	# placements = {"door" : (3, None)}
 	placements = {"footwell" : (2, None)}
 
+	# initialise placements
 	for placement in placements:
 		gpio_pin = placements[placement][0]
 		placements[placement] = (gpio_pin, gp.OutputDevice(gpio_pin, active_high=False, initial_value=False))
@@ -26,14 +32,13 @@ def is_valid_placement(placement):
 
 def toggle_relay(placement):
 	if not initialised:
-		print('initialising relay data')
 		setup_relay_data()
 
 	if is_valid_placement(placement):
 		relay_container = placements[placement]
 		gpio_pin = relay_container[0]
 		relay_container[1].toggle()
-		
+
 		print('switched relay chan {} on @ {}'.format(gpio_pin, placement))
 	else:
 		return
